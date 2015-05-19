b809868b3d5eab9319a09f41492af5fd81f31342
Joseph <jsu800@gmail.com>
2015-05-19 12:37:23 -0700

    reorganized data files and added source files

--- bin/run.sh
+++ bin/run.sh
@@ -5,4 +5,4 @@ javac -classpath .:../lib/jxl.jar:../lib/mail.jar:../lib/gson-2.1.jar ../src/Wri
 
 mv ../src/*.class .
 
-java -classpath .:../lib/jxl.jar:../lib/mail.jar:../lib/gson-2.1.jar MobileAnalytics mobileappsupport@nbcuni.com matt2237hew 80085056 Sales Daily Summary mobileapps@fandango.com F@nd@ng0mobile1 80070699 Sales Daily Summary 20120511 20120512 20120513 20120514 20120515
+java -classpath .:../lib/jxl.jar:../lib/mail.jar:../lib/gson-2.1.jar MobileAnalytics [ACCOUNT/CREDENTIALS] 80085056 Sales Daily Summary [ACCOUNT/CREDENTIALS] 80070699 Sales Daily Summary 20120511 20120512 20120513 20120514 20120515
